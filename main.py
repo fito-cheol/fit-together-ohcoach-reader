c20194c8ab9d07bb12c12987184adbc635f41c5a
Cheol_Kim <cheol.kim@fitogether.com>
2020-05-21 11:22:24 +0900

    Add Review

--- main.py
+++ main.py
@@ -11,14 +11,40 @@ class UserInterface:
         self.docking = Docking()
         self.file_save_path = []
 
+    # TODO parameter가 2개일 필요 없음 1개 쓸 것
+    # TODO return 값이 필요없는 함수
     def get_file_data_path(self, path, name):
         create_dir_if_not_exists(path, name)
         self.file_save_path.append(path + name)
         print(self.file_save_path)
         return self.file_save_path
 
+
+    # TODO file_save_path를 받는 이유가 없음
+    # TODO processing_dock은 return 값이 있을거라고 명시되어 있지 않지만 값을 return함
+    #      기능이 한 함수에 여러개가 들어가있으므로 분리할 것 - processing_dock, get_dock_status
     def processing_dock(self, file_save_path, line_process_index):
         each_lines_is_data_serial_num = self.docking.processing_dock(file_save_path, line_process_index)
+        # TODO 읽은 line과 안 읽은 line의 결과 값의 형식이 다름
+        #
+        '''
+         2번 줄만 읽었을 때 결과
+         
+                1번        2번        3번     4번     5번      6번
+              [[0, 0], [['5'], []], [0, 0], [0, 0], [0, 0], [0, 0]]
+              
+              문제1 
+              읽지 않은 line(Default)은 int, 
+              읽은 line은 string list or empty list
+              
+              문제2
+              CLBX-24434를 읽었었는데 결과값으로 5가 나왔음 시리얼번호가 맞는지 확인  
+              
+        '''
+        # TODO 실제로 돌려보기전에 어떤 값이 return이 될지 예상할 수 있도록 주석이 필요
+        # TODO each_lines_is_data_serial_num이라는 이름은
+        #      is~~가 들어가기 때문에 boolean값을 return할것으로 예상되지만 그렇지 않음
+        #      주석 먼저 달고나서 그에 맞게 이름을 고칠 필요가 있음
         return each_lines_is_data_serial_num
 
     def off_dock(self):
