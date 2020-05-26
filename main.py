1f9749c8bfc0ecd8b98f0d631d960ea0325adbb8
Cheol_Kim <cheol.kim@fitogether.com>
2020-05-26 16:06:58 +0900

    Clean up Comments and Test Execution

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import pprint
 
-from .common.ohcoach_reader_constants import *
 from .module.Docking import Docking
 
 
@@ -20,14 +19,14 @@ class UserInterface:
         self.create_dir_if_not_exists(dir_path)
         self._file_save_path = dir_path
 
+    '''
+        지정된 line만 processing 한다.
+        line_process_index = 0~5 (line 1~6)
+        line_process_index = -1 이면 전체 line processing
+    '''
     def processing_dock(self, line_process_index):
         self.docking.processing_dock(self._file_save_path, line_process_index)
-        '''
-              문제2
-              CLBX-24434를 읽었었는데 결과값으로 5가 나왔음 시리얼번호가 맞는지 확인  
-        '''
 
-    # TODO 실제로 돌려보기전에 어떤 값이 return이 될지 예상할 수 있도록 주석이 필요
     def get_open_closed_serial(self):
         return self.docking.open_closed_serial_per_line
 
@@ -43,30 +42,10 @@ class UserInterface:
 if __name__ == '__main__':
     objectUI = UserInterface()
 
-
-    # 지정된 line만 processing 한다.
-    # line_process_index = 0~5 (line 1~6)
-    # line_process_index = -1 이면 전체 line processing
     line_process_index = -1
 
-    # path = PATH_FROM_UI -> PATH_FROM_UI = C:/~/
-    path = ''
-    file_save_path = objectUI.get_file_data_path(path, NAME_DATA_SAVE_DIR)
-
-    '''
-    each_lines_is_data_serial_num 예시
-    
-    each_lines_is_data_serial_num = 
-    Line 1(총 6개 cell 중 4개 yes data 2개 no data) = [[['24348', '65507', '65500', '3'], ['65507', '3']],
-    Line 2(총 3개 중 3개 yes data) = [['26323', '14392', '13875'], []], 
-    Line 3 = [[], []],
-    Line 4 = [[], []],
-    Line 5 = [[], []],
-    Line 6 = [[], []]]
-    
-    
-    '''
-    each_lines_is_data_serial_num = objectUI.processing_dock(file_save_path, line_process_index)
+    objectUI.processing_dock(line_process_index)
+    each_lines_is_data_serial_num = objectUI.get_open_closed_serial()
     pprint.pprint(each_lines_is_data_serial_num)
 
     objectUI.off_dock()
